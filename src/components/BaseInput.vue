16170c69299903654e87195106f799af099deda7
Shako <shakokarwan@protonmail.com>
2026-05-20 00:17:09 +0300

    new login page

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="w-full px-3 py-1 rounded-[8px] bg-[#D9D9D9] border-2 border-[#1F1F1F] focus:outline-none focus:border-[#D9D9D9]"
+  />
+</template>
+
+<script setup>
+defineProps({
+  modelValue: String,
+  type: { type: String, default: 'text' },
+  placeholder: String,
+})
+defineEmits(['update:modelValue'])
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,30 @@
+<script setup>
+import { ref, watch } from 'vue'
+import BaseInput from '@/components/BaseInput.vue'
+const username = ref('')
+</script>
 <template>
-  <div class="bg-red-500 text-white p-10 text-4xl font-bold text-center">Tailwind is working!</div>
-
-  <!-- The rest of your app -->
+  <div class="h-screen flex bg-[#D9D9D9]">
+    <div
+      class="animate-slide-in-left shadow-xl 0.5s ease-out forwards h-screen w-[22vw] max-w-md bg-[#1F1F1F] rounded-tr-[14px] rounded-br-[14px] flex justify-center items-center flex-col px-12 gap-[20px]"
+    >
+      <h1 class="text-white text-2xl font-semibold">Sign in</h1>
+      <BaseInput v-model="username" placeholder="Username" />
+      <BaseInput v-model="password" type="password" placeholder="Password" />
+    </div>
+  </div>
 </template>
+<style>
+@keyframes slide-in-left {
+  from {
+    transform: translateX(-40px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+.animate-slide-in-left {
+  animation: slide-in-left 0.6s ease-out forwards;
+}
+</style>
