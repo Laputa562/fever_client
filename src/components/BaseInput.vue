174d50b812bf8a94c6a806f7b312f63680c60bdb
Shako <shakokarwan@protonmail.com>
2026-05-20 02:35:33 +0300

    Added domine font and log in button

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -4,7 +4,7 @@
     :placeholder="placeholder"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
-    class="w-full px-3 py-1 rounded-[8px] bg-[#D9D9D9] border-2 border-[#1F1F1F] focus:outline-none focus:border-[#D9D9D9]"
+    class="w-full px-3 py-2 rounded-[8px] bg-[#D9D9D9] border-2 border-[#1F1F1F] focus:outline-none focus:border-[#D9D9D9]"
   />
 </template>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,21 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import BaseInput from '@/components/BaseInput.vue'
 const username = ref('')
 </script>
 <template>
-  <div class="h-screen flex bg-[#D9D9D9]">
+  <div class="h-screen flex bg-[#D9D9D9] dark:bg-[#1F1F1F]">
     <div
-      class="animate-slide-in-left shadow-xl 0.5s ease-out forwards h-screen w-[22vw] max-w-md bg-[#1F1F1F] rounded-tr-[14px] rounded-br-[14px] flex justify-center items-center flex-col px-12 gap-[20px]"
+      class="animate-slide-in-left shadow-xl 0.5s ease-out forwards h-screen w-[22vw] max-w-md bg-[#1F1F1F] dark:bg-[#111111] rounded-tr-[14px] rounded-br-[14px] flex justify-center items-center flex-col px-12 gap-[20px]"
     >
-      <h1 class="text-white text-2xl font-semibold">Sign in</h1>
+      <h1 class="font-domine text-white text-3xl font-semibold">Log in</h1>
       <BaseInput v-model="username" placeholder="Username" />
       <BaseInput v-model="password" type="password" placeholder="Password" />
+      <button
+        class="font-domine mt-4 bg-[#D9D9D9] text-[#1F1F1F] py-2 w-full rounded-[14px] text-[17px] hover:bg-[#cccaca] active:scale-99 transition-transform duration-80 ease-out active:opacity-85"
+      >
+        Log in
+      </button>
     </div>
   </div>
 </template>
